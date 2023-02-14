572bb370db5c4a14f25ed2b00b2bb70355955f78
Vinicius Silva, Héredes <heredes.vinicius@gmail.com>
2023-02-14 16:20:11 -0300

    Criando Repository e Service para Categoria

---
+++ src/main/java/com/heredesvinicius/cursomc/repositories/CategoriaRepository.java
@@ -0,0 +1,9 @@
+package com.heredesvinicius.cursomc.repositories;
+
+import com.heredesvinicius.cursomc.domain.Categoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+}
---
+++ src/main/java/com/heredesvinicius/cursomc/services/CategoriaService.java
@@ -0,0 +1,20 @@
+package com.heredesvinicius.cursomc.services;
+
+import com.heredesvinicius.cursomc.domain.Categoria;
+import com.heredesvinicius.cursomc.repositories.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class CategoriaService {
+    @Autowired
+    private CategoriaRepository repo;
+
+    public Categoria find(Integer id) {
+        Optional<Categoria> obj = repo.findById(id);
+        return obj.orElse(null);
+    }
+
+}
